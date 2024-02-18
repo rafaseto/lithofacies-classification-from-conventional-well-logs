591209933d08b7ea41ff480de2a6be1f99ecaaf3
Rafael Takeguma <rafael08.0807@gmail.com>
2024-02-17 21:35:34 -0300

    atualiza funcao nphi_transform em aux_datapreprocess

--- aux_datapreprocess.py
+++ aux_datapreprocess.py
@@ -86,9 +86,9 @@ def NPHItransform(dlis_df):
     return dlis_df
 
 # Modelo da func. nova
-def nphi_transform(dlis_df, indexes):
-    for i in indexes:
-        dlis_df[i]["NPHI"] *= 100
+def nphi_transform(dlis_df_dict, pocos):
+    for poco in pocos:
+        dlis_df_dict[poco]["NPHI"] *= 100
 
 '''
 Exemplo de uso:
@@ -124,12 +124,12 @@ def remove_pontos_com_falha(dataframes, pontos_com_falha):
 '''
 # Defina condições de profundidade para cada dataframe
 pontos_com_falha = [
-    (dataframes[0]['profundidade '] < 250) | (dataframes[0]['profundidade '] >= 360),
-    (dataframes[1]['profundidade '] <= 75) | (dataframes[1]['profundidade '] >= 210),
-    (dataframes[2]['profundidade '] <= 60) | (dataframes[2]['profundidade '] >= 260),
-    (dataframes[3]['profundidade '] <= 90) | (dataframes[3]['profundidade '] >= 300),
-    (dataframes[4]['profundidade '] <= 64) | (dataframes[4]['profundidade '] >= 215),
-    (dataframes[5]['profundidade '] <= 90) | (dataframes[5]['profundidade '] >= 320)
+    (dataframes['1847']['profundidade '] < 250) | (dataframes['1847']['profundidade '] >= 360),
+    (dataframes['1848']['profundidade '] <= 75) | (dataframes['1848']['profundidade '] >= 210),
+    (dataframes['1851']['profundidade '] <= 60) | (dataframes['1851']['profundidade '] >= 260),
+    (dataframes['1853']['profundidade '] <= 90) | (dataframes['1853']['profundidade '] >= 300),
+    (dataframes['1855']['profundidade '] <= 64) | (dataframes['1855']['profundidade '] >= 215),
+    (dataframes['1857']['profundidade '] <= 90) | (dataframes['1857']['profundidade '] >= 320)
 ]
 
 # Chamando a função
